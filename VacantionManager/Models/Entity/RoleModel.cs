c25db5c3d9aa0f0bd287cdbef3d329e2bb2d6669
luchezar lubchov <lucho2002.1996@abv.bg>
2021-03-02 19:58:24 +0200

    Users and roles

---
+++ VacantionManager/Models/Entity/RoleModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VacantionManager.Models.Entity
+{
+    public class RoleModel
+    {
+        public RoleModel()
+        {
+            this.users = new HashSet<UserModel>();
+        }
+
+        [Key]
+        [Column(TypeName = "int")]
+        public int id { get; set; }
+
+        [Column(TypeName = "nvarchar(200)")]
+        [Required]
+        [Display(Name = "Enter name:")]
+        public string name { get; set; }
+
+        public virtual ICollection<UserModel> users { get; set; }
+    }
+}
---
+++ VacantionManager/Models/Entity/UserModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VacantionManager.Models.Entity
+{
+    public class UserModel
+    {
+        [Key]
+        [Column(TypeName = "int")]
+        public int id { get; set; }
+
+        [Column(TypeName = "nvarchar(200)")]
+        [Required]
+        [Display(Name ="Enter username:")]
+        public string username { get; set; }
+
+        [Column(TypeName = "nvarchar(200)")]
+        [Required]
+        [Display(Name = "Enter first name:")]
+        public string firstName { get; set; }
+
+        [Column(TypeName = "nvarchar(200)")]
+        [Required]
+        [Display(Name = "Enter last name:")]
+        public string lastName { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(max)")]
+        [Display(Name = "Enter password name:")]
+        public string password { get; set; }
+
+        [NotMapped]
+        [Required]
+        public string confirmPassword { get; set; }
+
+        [ForeignKey("RoleModel")]
+        [Required]
+        public RoleModel role { get; set; }
+
+        [ForeignKey("TeamModel")]
+        [Required]
+        public TeamModel team { get; set; }
+
+
+    }
+}
