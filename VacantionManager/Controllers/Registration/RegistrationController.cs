593cb625abd582d64dc66de956bbc71a848ea856
luchezar lubchov <lucho2002.1996@abv.bg>
2021-03-04 18:33:55 +0200

    Registration screen

---
+++ VacantionManager/Controllers/Registration/RegistrationController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VacantionManager.Models.Entity;
+using VacantionManager.Models;
+
+namespace VacantionManager.Controllers.Registration
+{
+    public class RegistrationController : Controller
+    {
+        private readonly VacantionManagerDBContext _context;
+
+        public RegistrationController(VacantionManagerDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Register user
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("id,username,firstName,lastName,password")] UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userModel);
+        }
+    }
+}
--- VacantionManager/Models/Entity/UserModel.cs
+++ VacantionManager/Models/Entity/UserModel.cs
@@ -22,27 +22,32 @@ namespace VacantionManager.Models.Entity
         public int id { get; set; }
 
         [Column(TypeName = "nvarchar(200)")]
-        [Required]
+        [Required(ErrorMessage = "Username is required field!")]
         [Display(Name ="Enter username:")]
         public string username { get; set; }
 
         [Column(TypeName = "nvarchar(200)")]
-        [Required]
+        [Required(ErrorMessage = "First name is required! field")]
         [Display(Name = "Enter first name:")]
         public string firstName { get; set; }
 
         [Column(TypeName = "nvarchar(200)")]
-        [Required]
+        [Required(ErrorMessage = "Last name is required field!")]
         [Display(Name = "Enter last name:")]
         public string lastName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password is required field!")]
         [Column(TypeName = "nvarchar(max)")]
-        [Display(Name = "Enter password name:")]
+        [Display(Name = "Enter password")]
+        [MinLength(8)]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}", ErrorMessage = "Password format doesn't match!\nIt must contains one uppercase and one lowercase letter, one symbol,\none number and it must be at least 8 symbols long!")]
         public string password { get; set; }
 
         [NotMapped]
-        [Compare("password")]
+        [Compare("password",ErrorMessage ="Passwords must match!")]
+        [Display(Name = "Confirm your password:")]
+        [MinLength(8)]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}", ErrorMessage = "Password format doesn't match!\nIt must contains one uppercase and one lowercase letter, one symbol,\none number and it must be at least 8 symbols long!")]
         public string confirmPassword { get; set; }
 
         [Required]
