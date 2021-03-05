17c4320aa067968c5403d0e34df6645708ffb6c4
luchezar lubchov <lucho2002.1996@abv.bg>
2021-03-05 23:40:28 +0200

    Register view logic done..

--- VacantionManager/Controllers/Registration/RegistrationController.cs
+++ VacantionManager/Controllers/Registration/RegistrationController.cs
@@ -13,12 +13,12 @@ namespace VacantionManager.Controllers.Registration
         private readonly VacantionManagerDBContext _context;
 
         public RegistrationController(VacantionManagerDBContext context)
-        {
+        {           
             _context = context;
         }
 
         public IActionResult Index()
-        {
+        {           
             return View();
         }
 
@@ -27,21 +27,29 @@ namespace VacantionManager.Controllers.Registration
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register([Bind("username,firstName,lastName,password,confirmPassword")] UserModel userModel)
+        public async Task<IActionResult> Index([Bind("username,firstName,lastName,password,confirmPassword")] UserModel userModel)
         {         
             if (ModelState.IsValid)
             {
-                if (!_context.Users.Select(u=>u.username).Contains(userModel.username))
+                if (!_context.Users.Select(u => u.username).Contains(userModel.username))
                 {
                     userModel.role = _context.Roles.Where(r => r.name == "Unassigned").FirstOrDefault();
                     userModel.password = Utilities.HashFunctions.HashPassword(userModel.password);
                     _context.Add(userModel);
                     await _context.SaveChangesAsync();
-                    return RedirectToAction(nameof(Index));
+                    return View();
                 }
-
+                else
+                {
+                    ViewData["Error message"] = "This username is taken!";
+                    return View();
+                }
             }
-            return RedirectToAction("Index", "Registration");
+            else 
+            { 
+                    ViewData["Error message"] = "One or more field has data that doesn't match the criteria for it!";
+                    return View();
+            }
         }
     }
 }
